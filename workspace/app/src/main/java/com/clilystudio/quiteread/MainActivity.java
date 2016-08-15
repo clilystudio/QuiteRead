dff819921166e1b2bf4601eec920a8959ede4358
clilystudio <bullsgl@gmail.com>
2016-08-15 17:34:47 +0800

    Start

---
+++ workspace/app/src/main/java/com/clilystudio/quiteread/MainActivity.java
@@ -0,0 +1,116 @@
+package com.clilystudio.quiteread;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+    private ListView mListView;
+    private ArrayList<String> list = new ArrayList<>();
+    private FloatingActionButton mFab;
+    private FloatingActionButton mFab2;
+    private NavigationView mNavView;
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer != null && drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        if (mDrawerLayout != null) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        return true;
+    }
+
+    private ArrayList<String> getData() {
+        list.add("Hello");
+        list.add("This is stormzhang");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        list.add("Hello");
+        list.add("This is stormzhang");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        list.add("Hello");
+        list.add("This is stormzhang");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        return list;
+    }
+
+    private void initViews() {
+        mListView = (ListView) findViewById(R.id.listview);
+        mListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getData()));
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        assert mFab != null;
+        mFab.setOnClickListener(this);
+        mFab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        assert mFab2 != null;
+        mFab2.setOnClickListener(this);
+        mNavView = (NavigationView) findViewById(R.id.nav_view);
+        assert mNavView != null;
+        mNavView.setNavigationItemSelectedListener(this);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                break;
+            case R.id.fab2:
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                break;
+        }
+    }
+}
